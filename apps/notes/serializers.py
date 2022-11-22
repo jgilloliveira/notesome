6cdc4b1659b8d7516ea2405a62dfd9c38c7524ee
Jacqueline Gill <jgilloli@gmail.com>
2022-11-21 22:07:31 -0300

    Lectura y escritura de categorias

--- apps/notes/serializers.py
+++ apps/notes/serializers.py
@@ -1,5 +1,6 @@
 from email.policy import default
 from api_rest.serializers import BaseModelSerializer
+from apps.categories.models import Category
 from apps.notes.models import Note
 from rest_framework import serializers
 from apps.categories.serializers import CategorySerializer
@@ -8,7 +9,8 @@ from apps.categories.serializers import CategorySerializer
 class NoteSerializer(BaseModelSerializer):
 
   user = serializers.HiddenField(default=serializers.CurrentUserDefault())
-  categories = CategorySerializer(many=True, required=False)
+  categories = CategorySerializer(many=True, required=False, read_only=True)
+  new_categories = serializers.ListField(required=False, write_only=True, source="categories")
 
   class Meta:
     model = Note
